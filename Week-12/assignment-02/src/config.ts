0560b0c56448989c90ba10c112bce5b2cf09032f
KhushmanPreet <ninjasingh12@gmail.com>
2026-06-16 00:00:30 +0530

    week-12-assignment 1 and 2 no

--- Week-12/assignment-02/src/config.ts
+++ Week-12/assignment-02/src/config.ts
@@ -1,2 +1,2 @@
 
-export const DB_URL = "postgresql://neondb_owner:npg_vFe3lDS5cJOV@ep-quiet-shape-aix701uq-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
+export const DB_URL = "";
